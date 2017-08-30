052479dda67d51dbd8742e3de5e2e91d696159bf
Sigit setiaji <sigitsetiaji4@gmail.com>
2017-08-30 15:34:41 +0700

    Ubah Jadi versi paling awal

--- runserver.py
+++ runserver.py
@@ -3,7 +3,7 @@ This script runs the Sparse application using a development server.
 """
 
 from os import environ
-from Desi import app
+from Sigit import app
 
 #if __name__ == '__main__':
 #    HOST = environ.get('SERVER_HOST', 'localhost')
